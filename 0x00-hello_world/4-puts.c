50280170ee80c8d5c49ae59852f45442e478325c
edwardmuss <edwardmuss5@gmail.com>
2021-06-17 06:12:14 -0700

    Print Programming is like building...

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * Language: C
+ * @EdwardMuss
+ */
+
+#include <stdio.h>
+
+/**
+ * prints "Programming is like building a multilingual puzzle", followed by a new line.
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
