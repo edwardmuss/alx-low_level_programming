7ae83e20aa49f5639202502d4a461bb649d155d1
edwardmuss <edwardmuss5@gmail.com>
2021-06-28 21:02:32 +0300

    Fix Kernel Style Code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,13 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (s[len] != '\0') {
+	while (s[len] != '\0') 
+	{
 		len++;
 	}
 
-	for (len -= 1; len >= 0; len--){
+	for (len -= 1; len >= 0; len--)
+	{
 		_putchar(s[len]);
 	}
 		
