1cbb5567d005ba15b2cf8b7b2eedba8c2421f74d
edwardmuss <edwardmuss5@gmail.com>
2021-08-03 10:16:54 +0300

    header modify

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef _S_LINK_LISTS_
-#define _S_LINK_LISTS_
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -7,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton/ALX project
+ * for Holberton/ALX  project
  */
 typedef struct listint_s
 {
@@ -27,9 +28,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
