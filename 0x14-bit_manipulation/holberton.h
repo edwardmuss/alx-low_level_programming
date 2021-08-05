46d70fb4822da0a97c7b0890fcb66ea338517611
edwardmuss <edwardmuss5@gmail.com>
2021-08-05 21:17:25 +0300

    correct header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,19 +1,14 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _BIT_MANIP_
+#define _BIT_MANIP_
 
-/*
- * File: holberton.h
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-bit_manipulation directory.
- */
-
-int _putchar(char c);
+int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
+void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
 
-#endif /* HOLBERTON_H */
+#endif
