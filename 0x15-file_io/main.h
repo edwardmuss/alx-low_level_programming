56c501e0312e0e886d4dcb0eb72517f2f948a355
edwardmuss <edwardmuss5@gmail.com>
2021-08-09 15:27:47 +0300

    file io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
